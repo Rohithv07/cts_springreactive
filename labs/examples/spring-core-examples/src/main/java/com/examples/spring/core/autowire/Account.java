eabaaf3da425757007e7cd3666e3bea1b0ece8cd
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-05-24 11:40:00 +0530

    spring core examples

---
+++ labs/examples/spring-core-examples/src/main/java/com/examples/spring/core/autowire/Account.java
@@ -0,0 +1,17 @@
+package com.examples.spring.core.autowire;
+
+public class Account {
+	
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
