3490905ea5c60f381c2ed91ac56f29d1508d3533
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-05-26 10:20:28 +0530

    employee reactive service updates

--- labs/examples/spring-reactive-rest-employee-api/src/main/java/com/examples/spring/webflux/service/EmployeeService.java
+++ labs/examples/spring-reactive-rest-employee-api/src/main/java/com/examples/spring/webflux/service/EmployeeService.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Mono;
 
 public interface EmployeeService {
 	public Flux<Employee> getAllEmployees();
-//	public Mono<Employee> getEmployee(Integer empId);
-
+	public Mono<Employee> getEmployee(Integer empId);
+	public Mono<Employee> createEmployee(Employee employee);
+	public void updateEmployee(Employee employee);
+	public void deleteEmployee(Integer empId);
 }
