4d38ab7a3a0cedca9ac4c0ca5ceea91edb6b1f05
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-06-01 11:31:16 +0530

    RabbitMQ pubsub example

---
+++ labs/examples/spring-boot-rabbitmq-pubsub/src/main/java/com/examples/messaging/rabbitmq/Application.java
@@ -0,0 +1,46 @@
+package com.examples.messaging.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    static final String topicExchangeName = "demo-exchange";
+
+    static final String queueName = "greetings";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+    	return BindingBuilder.bind(queue).to(exchange).with("demo.greeting.#");
+    }
+    
+    @Bean
+    public Receiver receiver()
+    {
+    	return new Receiver();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args).close();
+    }
+
+}
