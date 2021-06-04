426648047b33b844486a9082a4718033f2394b8f
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-06-04 11:12:12 +0530

    employee service with integration test cases

---
+++ labs/examples/spring-reactive-test-employee-api/src/main/java/com/examples/spring/webflux/SpringReactiveTestEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.examples.spring.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveTestEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveTestEmployeeApiApplication.class, args);
+	}
+
+}
