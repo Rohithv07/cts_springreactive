32fab392d6b5d9b1c8d2bd8b082553dd8b2477a5
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-08-18 11:43:41 +0530

    oauth2 jwt examples

---
+++ labs/examples/spring-boot-security-oauth2/src/test/java/com/examples/boot/security/SpringBootSecurityOauth2AuthApplicationTests.java
@@ -0,0 +1,14 @@
+package com.examples.boot.security;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootSecurityOauth2AuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
